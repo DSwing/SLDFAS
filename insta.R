7660b9c53a631d4dafa7fd142c316c992f1afee4
DSwing <guadalupo.do@gmail.com>
2023-08-21 09:34:51 +0100

    installation file

---
+++ insta.R
@@ -0,0 +1,22 @@
+# set working directory so I know where the .zip file will be located
+dir.create("sld_fas_repo_dir")
+setwd(dir = "sld_fas_repo_dir")
+
+# download a .zip file of the repository
+# from the "Clone or download - Download ZIP" button
+# on the GitHub repository of interest
+download.file(url = "https://github.com/DSwing/SLDFAS/archive/refs/heads/master.zip"
+              , destfile = "sld_fas.zip")
+
+# unzip the .zip file
+unzip(zipfile = "sld_fas.zip")
+
+# set the working directory
+# to be inside the newly unzipped 
+# GitHub repository of interest
+setwd(dir = "SLDFAS-master")
+
+openProject(path = "sld-fas.Rproj")
+renv::restore()
+
+install.packages("arcgisbinding_1.0.1.300.zip", repos=NULL)
